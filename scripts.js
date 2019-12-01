20f8fea6b07a05c7c0cb3b9ab5bdc4c9f3e5d395
Sarah <ms.sarah.adkins@gmail.com>
2019-12-01 15:19:24 -0800

    Added current time to application

--- scripts.js
+++ scripts.js
@@ -1,2 +1,20 @@
+let api = 'http://52.88.188.196:8080/api/api/where/';
+let key = '?key=TEST';
+
 console.log(`👩`);
-// Set intrval
+time();
+
+function time() {
+
+    $.get(`${api}current-time.json${key}`, function (data) {
+        let curtime = new Date(data.data.entry.readableTime);
+        let hours = curtime.getHours()
+        if (hours > 12) {
+            hours = hours - 12;
+        }
+        $('#app').append(`<h1>${hours}:${curtime.getMinutes()}</h1>`);
+    }, "jsonp");
+    return time;
+}
+
+// How to update -> setintervals
