cb2903c451a2a814223373ee195c18bd6deabe32
Sarah <ms.sarah.adkins@gmail.com>
2019-12-02 22:13:39 -0800

    Added alert

--- scripts.js
+++ scripts.js
@@ -12,8 +12,8 @@ function start() {
     getStops();
     console.log(`👩`);
     runApp();
-    let timerId = setInterval(() => runApp(), 30000); //30 sec
-    setTimeout(() => { clearInterval(timerId); alert('Clock Stopped'); }, 60000); //One Min
+    // let timerId = setInterval(() => runApp(), 30000); //30 sec
+    // setTimeout(() => { clearInterval(timerId); alert('Clock Stopped'); }, 60000); //One Min
     $("#updateStyle").click(updateStyles);
 }
 
@@ -29,7 +29,7 @@ function updateStyles() {
     scolor = $("#scolor").val();
     imageurl = $("#imageurl").val();
     $("body").css("color", `#${pcolor}`);
-    $("#curtime").css("color", `#${pcolor}`);
+    $("div.jumbotron").css("color", `#${pcolor}`);
     $("footer").css("color", `#${scolor}`);
     $("#logo").replaceWith(`<img id="logo" src="${imageurl}" alt="logo">`);
 }
@@ -39,6 +39,7 @@ function Displaytime() {
         let curtime = formatTime(data.data.entry.time);
         $('#app').append(`
         <div class="jumbotron">
+        <h1>${stopID}</h1>
         <h1 id="curtime">${curtime}</h1>
         </div>
         `);
@@ -59,16 +60,18 @@ function formatTime(humanreadble) {
 }
 
 function getTable() {
+    // <th scope="col">Departure Time(if different)</th>
     $.get(`${api}schedule-for-stop/${stopID}.json${key}`, function (data) {
         let schedule = data.data.entry.stopRouteSchedules;
         $('#app').append(`
         <table class="table table-bordered">
-        <thead>
+        <thead id="nextBus">
           <tr>
+          <th scope="col">Stops aways</th>
             <th scope="col">Route #</th>
-            <th scope="col">Arrival</th>
-            <th scope="col">Departure</th>
+            <th scope="col">Scheduled/Estimated Arrival Time</th>
             <th scope="col">Status</th>
+            
           </tr>
         </thead>
         <tbody id="routesTable">
@@ -76,23 +79,22 @@ function getTable() {
         </tbody>
         </table>
         `);
+        getArrival();
         for (let i = 0; i < schedule.length; i++) {
             // let time = Math.round(schedule[i].stopRouteDirectionSchedules[0].scheduleStopTimes[0].arrivalTime.getTime() / 1000);
             let arrivaltime = new Date(schedule[i].stopRouteDirectionSchedules[0].scheduleStopTimes[0].arrivalTime * 1000.0);
             let departuretime = new Date(schedule[i].stopRouteDirectionSchedules[0].scheduleStopTimes[0].departureTime * 1000.0);
-
+            console.log(schedule[i]);
             $('#routesTable').append(`
             <tr>
+            <td></td>
                 <th scope="row">${schedule[i].routeId} ${schedule[i].stopRouteDirectionSchedules[0].tripHeadsign}</th>
                 <td>${formatTime(arrivaltime)}</td>
-                <td>${formatTime(departuretime)}</td>
-                <td>@twitter</td>
+                <td>Scheduled</td>
             </tr>
             `);
         }
-        $("#app").append(`
 
-        `);
     }, "jsonp");
 }
 //http://52.88.188.196:8080/api/api/where/routes-for-agency/STA.json?key=TEST
@@ -110,3 +112,49 @@ function getStops() {
 // How to update -> setintervals
 // Departure Times are the intrested and the Predicted Departure/Arrival/?Actual? Time 
 // Promises
+
+function getArrival() {
+    $.get(`${api}arrivals-and-departures-for-stop/${stopID}.json${key}`, function (data) {
+        if (data.data.entry.arrivalsAndDepartures[0] == null) {
+            $('#nextBus').append(`
+            <tr  class="bg-danger">
+                <th colspan="4"> No Data On Next Bus </th>
+            </tr>
+            `);
+        } else {
+            let nextBus = data.data.entry.arrivalsAndDepartures[0];
+            let arrivalTime = formatTime(nextBus.predictedArrivalTime);
+            let status = nextBus.tripStatus.status
+            if (nextBus.tripStatus.status == 'SCHEDULED') {
+                status = 'On Time';
+            }
+            if (nextBus.numberOfStopsAway < 1) {
+                console.log('pop');
+                popUp();
+            }
+            $('#nextBus').append(`
+            <tr  class="bg-warning">
+            <td>${nextBus.numberOfStopsAway}</td>
+                <th scope="row">${nextBus.routeShortName} - ${nextBus.routeLongName}</th>
+                <td>${arrivalTime}</td>
+                <td>${status}</td>
+                
+            </tr>
+            `);
+        }
+
+    }, "jsonp");
+
+}
+
+function popUp() {
+    console.log('up');
+
+    $('.jumbotron').append(`
+    <div class="alert alert-warning alert-dismissible fade in">
+    <audio autoplay><source src="mysound.mp3" type="audio/mpeg" /></audio>
+        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+        <strong>Warning!</strong> This alert box could indicate a warning that might need attention.
+    </div>
+`);
+}
